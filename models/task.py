abe890fd35f09ab29d8e13ad9a63737b53a376b5
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:42:35 +0300

    project.py

--- models/task.py
+++ models/task.py
@@ -1,1 +1,10 @@
 from db.connection import CONN, CURSOR
+
+class Task:
+    def __init__(self, title, project_id, status="Pending", id=None):
+        self.id = id
+        self.title = title
+        self.status = status
+        self.project_id = project_id
+
+        
