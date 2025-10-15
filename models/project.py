18bbb3f8cbe67ce63ec3399cc87b5689df445963
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:41:42 +0300

    project.py

--- models/project.py
+++ models/project.py
@@ -11,19 +11,19 @@ class Project:
         CURSOR.execute("INSERT INTO projects (name, user_id) VALUES (?, ?)", (name, user_id))
         CONN.commit()
         return cls(name, user_id, CURSOR.lastrowid)
-  
+
     @classmethod
     def get_all(cls):
         CURSOR.execute("SELECT * FROM projects")
         rows = CURSOR.fetchall()
         return [cls(*row[1:], id=row[0]) for row in rows]
-    
+
     @classmethod
     def find_by_id(cls, id):
         CURSOR.execute("SELECT * FROM projects WHERE id=?", (id,))
         row = CURSOR.fetchone()
         return cls(*row[1:], id=row[0]) if row else None
-    
+
     def delete(self):
         CURSOR.execute("DELETE FROM projects WHERE id=?", (self.id,))
         CONN.commit()
