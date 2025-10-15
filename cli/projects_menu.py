dac0340c4c93d6c11f5df1780088098555ec9a0b
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 22:02:32 +0300

    users-menu.py

--- cli/projects_menu.py
+++ cli/projects_menu.py
@@ -1,2 +1,12 @@
 from models.project import Project
 
+def projects_menu():
+    while True:
+        print("\n--- Projects Menu ---")
+        print("1. Add Project")
+        print("2. View All Projects")
+        print("3. Find Project by ID")
+        print("4. Delete Project")
+        print("0. Back")
+
+        
