0e373cfb6cbca185aeb1ca6a87ac6a12baa3a7d6
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:37:42 +0300

    project.py

--- models/project.py
+++ models/project.py
@@ -1,1 +1,7 @@
 from db.connection import CONN, CURSOR
+
+class Project:
+    def __init__(self, name, user_id, id=None):
+        self.id = id
+        self.name = name
+        self.user_id = user_id
