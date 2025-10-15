a62917c1641996230e74296aaac7a3946a4667bf
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 17:56:27 +0300

    connection.py

--- db/connection.py
+++ db/connection.py
@@ -2,6 +2,7 @@ import sqlite3
 
 CONN = sqlite3.connect("taskmate.db")
 CURSOR = CONN.cursor()
+
 def initialize_db():
     """Create tables if they don't exist."""
     CURSOR.execute("""
@@ -11,7 +12,7 @@ def initialize_db():
         email TEXT UNIQUE NOT NULL
     )
     """)
-    
+
     CURSOR.execute("""
     CREATE TABLE IF NOT EXISTS projects (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,7 +22,7 @@ def initialize_db():
     )
     """)
 
-CURSOR.execute("""
+    CURSOR.execute("""
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
@@ -30,4 +31,5 @@ CURSOR.execute("""
         FOREIGN KEY (project_id) REFERENCES projects(id)
     )
     """)
-CONN.commit()
+
+    CONN.commit()
