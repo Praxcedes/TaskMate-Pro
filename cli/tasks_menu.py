0ee0810ddd71d86d83ab57007b28daa47388ebb6
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 22:16:21 +0300

    task-menu.py

--- cli/tasks_menu.py
+++ cli/tasks_menu.py
@@ -1,1 +1,13 @@
 from models.task import Task
+
+def tasks_menu():
+    while True:
+        print("\n--- Tasks Menu ---")
+        print("1. Add Task")
+        print("2. View All Tasks")
+        print("3. Find Task by ID")
+        print("4. Mark as Complete")
+        print("5. Delete Task")
+        print("0. Back")
+
+        
