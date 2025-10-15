ad3284b3f2b3442bc4cad3404ca7ef22f3727979
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:47:58 +0300

    project.py

--- models/task.py
+++ models/task.py
@@ -12,19 +12,19 @@ class Task:
         CURSOR.execute("INSERT INTO tasks (title, project_id) VALUES (?, ?)", (title, project_id))
         CONN.commit()
         return cls(title, project_id, id=CURSOR.lastrowid)
-    
+
     @classmethod
     def get_all(cls):
         CURSOR.execute("SELECT * FROM tasks")
         rows = CURSOR.fetchall()
         return [cls(*row[1:], id=row[0]) for row in rows]
-    
+
     @classmethod
     def find_by_id(cls, id):
         CURSOR.execute("SELECT * FROM tasks WHERE id=?", (id,))
         row = CURSOR.fetchone()
         return cls(*row[1:], id=row[0]) if row else None
-    
+
     def mark_complete(self):
         CURSOR.execute("UPDATE tasks SET status='Completed' WHERE id=?", (self.id,))
         CONN.commit()
@@ -33,5 +33,3 @@ class Task:
     def delete(self):
         CURSOR.execute("DELETE FROM tasks WHERE id=?", (self.id,))
         CONN.commit()
-
-    
