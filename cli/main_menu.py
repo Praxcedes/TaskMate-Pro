1e395e0f3d3615e8f26b42eaabaa174368b0c3ef
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:52:04 +0300

    main-menu.py

--- cli/main_menu.py
+++ cli/main_menu.py
@@ -2,3 +2,12 @@ from cli.users_menu import users_menu
 from cli.projects_menu import projects_menu
 from cli.tasks_menu import tasks_menu
 
+def main_menu():
+    while True:
+        print("\n=== TaskMate Pro Main Menu ===")
+        print("1. Manage Users")
+        print("2. Manage Projects")
+        print("3. Manage Tasks")
+        print("0. Exit")
+
+        
