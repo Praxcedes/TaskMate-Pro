6d2bf6bffeddbcd9559df5aaf3bf5e60d3ad6d12
Praxcedes <praxcedeskabeya@gmail.com>
2025-10-15 21:55:23 +0300

    users-menu.py

--- cli/users_menu.py
+++ cli/users_menu.py
@@ -1,1 +1,12 @@
 from models.user import User
+
+def users_menu():
+    while True:
+        print("\n--- Users Menu ---")
+        print("1. Add User")
+        print("2. View All Users")
+        print("3. Find User by ID")
+        print("4. Delete User")
+        print("0. Back")
+
+        
